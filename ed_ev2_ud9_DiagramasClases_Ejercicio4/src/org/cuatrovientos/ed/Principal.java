e08646a9075ef784466cb76b296b2fa062c9155d
kike <kike@DESKTOP-FV8SMGI>
2026-01-20 20:43:49 +0100

    Entrega Uml + java

---
+++ ed_ev2_ud9_DiagramasClases_Ejercicio4/src/org/cuatrovientos/ed/Principal.java
@@ -0,0 +1,10 @@
+package org.cuatrovientos.ed;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ed_ev2_ud9_DiagramasClases_Ejercicio4/src/org/cuatrovientos/ed/Product.java
@@ -0,0 +1,5 @@
+package org.cuatrovientos.ed;
+
+public class Product {
+
+}
